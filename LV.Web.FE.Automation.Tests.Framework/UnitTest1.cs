d6a0745fd18f51990c1bec05844f3b9b5bc11da8
hristo831 <hristo83111@abv.bg>
2017-06-27 19:51:41 +0300

    First Commit

---
+++ LV.Web.FE.Automation.Tests.Framework/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LV.Web.FE.Automation.Tests.Framework
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
